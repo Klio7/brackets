07c8ec4935f9b2c4aac0bbfe93071bac83e36fb1
Anna Odintsova <Klio7@mail.ru>
2023-02-26 20:22:23 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const pairs = bracketsConfig.map((bracketPair) => bracketPair.join(""));
+  let strLength = 0;
+
+  while (str.length !== strLength) {
+    strLength = str.length;
+    pairs.forEach((pair) => {
+      str = str.replace(pair, "");
+    });
+  }
+
+  return strLength === 0;
+};
